a042f5d845ebe39e2e7f01c10ffab661158485d5
Jacob MacDonald <jaccarmac@gmail.com>
2014-07-02 10:37:33 -0700

    Regress to old Compojure

--- unnecessary-maven/project.clj
+++ unnecessary-maven/project.clj
@@ -1,7 +1,8 @@
 (defproject unnecessary-maven "1.0.3"
   :description "Use non-Maven JVM build tools on App Engine."
-  :dependencies [^{:voom {:repo "https://github.com/weavejester/compojure"}}
-                 [compojure "1.1.8-20140510_232900-g1597cc8"]
+  :dependencies [^{:voom {:repo "https://github.com/weavejester/compojure"
+                          :branch "master"}}
+                 [compojure "1.2.0-20131030_015250-ge8b2abf"]
                  [org.clojure/clojure "1.6.0"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler unnecessary-maven.handler/app})
