02a1034474deab52ef184795162ab9f0b01c760a
Jacob MacDonald <jaccarmac@gmail.com>
2014-07-02 10:18:09 -0700

    Release 1.0.3

--- unnecessary-maven/project.clj
+++ unnecessary-maven/project.clj
@@ -1,4 +1,4 @@
-(defproject unnecessary-maven "1.0.2"
+(defproject unnecessary-maven "1.0.3"
   :description "Use non-Maven JVM build tools on App Engine."
   :dependencies [^{:voom {:repo "https://github.com/weavejester/compojure"}}
                  [compojure "1.1.8-20140510_232900-g1597cc8"]
